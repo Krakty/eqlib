37c5b55494d10e044a55dece820222135685d835
brainiac <brainiac2k@gmail.com>
2025-06-18 09:36:09 -0700

    Update for live patch

--- AltAbilities.h
+++ AltAbilities.h
@@ -31,7 +31,7 @@ struct AltAbilityEffectData
 	int slot;
 };
 
-constexpr size_t CAltAbilityData_size = 0xE0; // @sizeof(CAltAbilityData) :: 2025-06-09 (test) @ 0x1402107FD
+constexpr size_t CAltAbilityData_size = 0xE0; // @sizeof(CAltAbilityData) :: 2025-06-16 (live) @ 0x140210DED
 
 class [[offsetcomments]] CAltAbilityData
 {
